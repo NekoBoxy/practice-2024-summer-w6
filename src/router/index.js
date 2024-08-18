0b685a8a5dd430d1c2196f92537b85a58776db35
CatBoxy <catboxy@gmail.com>
2024-08-18 18:21:49 +0800

    基本設定、圖片新增

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,20 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
       name: 'home',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/HomeView.vue')
+      component: () => import('../views/IndexView.vue'),
+      children: [
+        {
+          path: '/',
+          name: 'home',
+          component: () => import('../views/HomeView.vue'),
+        },
+        // {
+        //   path: '/',
+        //   name: 'home',
+        //   component: () => import('../views/HomeView.vue'),
+        // },
+      ]
     },
   ]
 })
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,50 +1,15 @@
 <script setup>
 import { ref, onMounted } from "vue";
-// 法 1
-import { Modal } from 'bootstrap';
+// import { Modal } from 'bootstrap';
 
-// 法 2
-// import * as bootstrap from 'bootstrap'
-
-const myModal = ref(null);
+// const myModal = ref(null);
 
 onMounted(() => {
-  // 法 1
-  const test = new Modal(myModal.value);
-  test.show();
-
-  // 法 2
-  // const test = new bootstrap.Modal(myModal.value, {});
+  // const test = new Modal(myModal.value);
   // test.show();
-
 })
 </script>
 
 <template>
-  <main>
-    <!-- Button trigger modal -->
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-      Launch demo modal
-    </button>
-
-    <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true"
-      ref="myModal">
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            ...
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-            <button type="button" class="btn btn-primary">Save changes</button>
-          </div>
-        </div>
-      </div>
-    </div>
-  </main>
+  <div>首頁</div>
 </template>
